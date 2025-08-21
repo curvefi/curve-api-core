82c7de848b03b588b824742b64ad05f0232ffc0e
mimaklas <161614979+mimaklas@users.noreply.github.com>
2025-08-21 17:11:23 +0300

    Add cbBTC price mapping

--- constants/CoinAddressCoingeckoIdMap.js
+++ constants/CoinAddressCoingeckoIdMap.js
@@ -50,6 +50,7 @@ const HARDCODED_DATA = {
     '0x61D66bC21fED820938021B06e9b2291f3FB91945': 'ethereum', // wETH
     '0xAf368c91793CB22739386DFCbBb2F1A9e4bCBeBf': 'wrapped-steth', // wstETH
     '0xae4efbc7736f963982aacb17efa37fcbab924cb3': 'solv-btc', // SolvBTC
+    '0x7048c9e4aBD0cf0219E95a17A8C6908dfC4f0Ee4': 'coinbase-wrapped-btc', // cbBTC
   },
   'etherlink': {
     '0xecAc9C5F704e954931349Da37F60E39f515c11c1': 'lombard-staked-btc',
