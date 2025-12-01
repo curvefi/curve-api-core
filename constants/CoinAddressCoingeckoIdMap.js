e76090b1923782a9e0c4e0c8a3341424ad116ef4
mimaklas <161614979+mimaklas@users.noreply.github.com>
2025-12-02 01:35:30 +0200

    Add price mapping for BTC.b

--- constants/CoinAddressCoingeckoIdMap.js
+++ constants/CoinAddressCoingeckoIdMap.js
@@ -82,6 +82,7 @@ const HARDCODED_DATA = {
     '0xA3D68b74bF0528fdD07263c60d6488749044914b': 'wrapped-eeth', // weETH
     '0x0555E30da8f98308EdB960aa94C0Db47230d2B9c': 'wrapped-bitcoin', // WBTC
     '0xecAc9C5F704e954931349Da37F60E39f515c11c1': 'lombard-staked-btc', // LBTC
+    '0xB0F70C0bD6FD87dbEb7C10dC692a2a6106817072': 'bitcoin-avalanche-bridged-btc-b', // BTC.b
   },
 };
 
